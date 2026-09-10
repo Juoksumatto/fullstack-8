93bfebb980a56315111ab3e0c6896291d09225db
onni <holttaonni@gmail.com>
2026-09-10 12:36:30 +0300

    Add files via upload

---
+++ 8.10 tehtävä/fs-graphql-main/library-frontend/src/components/NewBook.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+
+const NewBook = (props) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [published, setPublished] = useState('')
+  const [genre, setGenre] = useState('')
+  const [genres, setGenres] = useState([])
+
+  if (!props.show) {
+    return null
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    const book = {
+      title, 
+      author, 
+      published: Number(published),
+      genres
+    }
+
+    await props.addBook(book)
+
+    setTitle('')
+    setPublished('')
+    setAuthor('')
+    setGenres([])
+    setGenre('')
+  }
+
+  const addGenre = () => {
+    setGenres(genres.concat(genre))
+    setGenre('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          <label htmlFor="title">title</label>
+          <input
+            id="title"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="author">author</label>
+          <input
+            id="author"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="published">published</label>
+          <input
+            id="published"
+            type="number"
+            value={published}
+            onChange={({ target }) => setPublished(target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="genre">genre</label>
+          <input
+            id="genre"
+            value={genre}
+            onChange={({ target }) => setGenre(target.value)}
+          />
+          <button onClick={addGenre} type="button">
+            add genre
+          </button>
+        </div>
+        <div>genres: {genres.join(' ')}</div>
+        <button type="submit">create book</button>
+      </form>
+    </div>
+  )
+}
+
+export default NewBook
