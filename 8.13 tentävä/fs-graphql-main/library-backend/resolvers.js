fc1544be63a339f0a87e54dae3684b6ca5997620
onni <holttaonni@gmail.com>
2026-09-16 09:45:32 +0300

    Add files via upload

---
+++ 8.13 tentävä/fs-graphql-main/library-backend/resolvers.js
@@ -0,0 +1,134 @@
+const { v1: uuid } = require('uuid')
+const { GraphQLError } = require('graphql')
+const Author = require('./modules/author')
+const Book = require('./modules/book')
+
+let authors = [
+  {
+    name: "Robert Martin",
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+  },
+  {
+    name: "Martin Fowler",
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963,
+  },
+  {
+    name: "Fyodor Dostoevsky",
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821,
+  },
+  {
+    name: "Joshua Kerievsky",
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  },
+  {
+    name: "Sandi Metz",
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  },
+]
+
+let books = [
+  {
+    title: "Clean Code",
+    published: 2008,
+    Author: "Robert Martin",
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Agile software development",
+    published: 2002,
+    Author: "Robert Martin",
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ["agile", "patterns", "design"],
+  },
+  {
+    title: "Refactoring, edition 2",
+    published: 2018,
+    Author: "Martin Fowler",
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Refactoring to patterns",
+    published: 2008,
+    Author: "Joshua Kerievsky",
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "patterns"],
+  },
+  {
+    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
+    published: 2012,
+    Author: "Sandi Metz",
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "design"],
+  },
+  {
+    title: "Crime and punishment",
+    published: 1866,
+    Author: "Fyodor Dostoevsky",
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "crime"],
+  },
+  {
+    title: "Demons",
+    published: 1872,
+    Author: "Fyodor Dostoevsky",
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "revolution"],
+  },
+]
+
+const resolvers = {
+  Query: {
+    booksCount: async () => Book.countDocuments(),
+    authorCount: async () => Author.countDocuments(),
+    allBooks: async () => {
+      return Book.find().populate('author')
+    },
+
+    allAuthors: async () => {
+      return Author.find()
+    }
+  },
+
+  Author: {
+    bookCount: (root) => {
+      return books.filter(book => book.Author === root.name).length
+  }
+  },
+
+  Mutation: {
+    addBook: async (root, args) => {
+      let author = await Author.findOne({ name: args.author })
+
+      if (!author) {
+        author = await Author.create({ name: args.author })
+      }
+
+      const book = new Book({
+        title: args.title,
+        published: args.published,
+        genres: args.genres,
+        author: author._id,
+      })
+
+      const savedBook = await book.save()
+      return savedBook.populate('author')
+    },
+    editAuthor: (root, args) => {
+    const author = authors.find(p => p.name === args.name)
+    if (!author) {
+      return null
+    }
+    const updatedAuthor = { ...author, born: args.setBornTo }
+    authors = authors.map(p => p.name === args.name ? updatedAuthor : p)
+    return updatedAuthor
+    },
+  },
+}
+
+
+module.exports = resolvers
---
+++ 8.13 tentävä/fs-graphql-main/library-backend/schema.js
@@ -0,0 +1,42 @@
+const typeDefs = /* GraphQL */`
+  type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String!]!
+    id: ID!
+  }
+
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int
+      genres: [String!]!
+    ): Book!
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
+  }
+
+  enum YesNo {
+    Yes
+    NO
+  }
+
+  type Query {
+    booksCount: Int
+    authorCount: Int
+    allBooks(author: String, genres: String): [Book!]!
+    allAuthors: [Author!]!
+  }
+`
+
+module.exports = typeDefs
